7970e397501e75024c3e845fa62f333854aac4ab
tom2tom <tpgww@onepost.net>
2022-04-26 14:50:29 +1000

    update licence text

--- method.uninstall.php
+++ method.uninstall.php
@@ -2,9 +2,9 @@
 /*
  * Module: ECB2 - Extended Content Blocks 2
  * Author: Chris Taylor
- * Copyright: (C) 2016-2022 Chris Taylor, chris@binnovative.co.uk
- * Licence: GNU General Public License version 3
- *          see /ECB2/lang/LICENCE.txt or <http: *www.gnu.org/licenses/>
+ * Copyright: (C) 2016-2022 CMS Made Simple Foundation
+ * Licence: GNU General Public License version 3 or later
+ *  see <https://www.gnu.org/licenses/gpl-3.0.txt>
  */
 if (!isset($gCms)) exit;
 
